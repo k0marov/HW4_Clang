d826de2780764abdaec0a731425fa81b2a3506f3
Sam Komarov <sam@skomarov.com>
2023-04-22 13:55:35 +0300

    solve task #1

---
+++ task_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int month_length(int month, int year) {
+  if (month == 2) {
+    int is_leap = !(year % 4) && (year % 100 || !(year % 400)); 
+    return is_leap ? 29 : 28; 
+  } else if (month == 4 || month == 6 || month == 9) {
+    return 30; 
+  } else {
+    return 31; 
+  }
+}
+
+
+int main() { 
+  int month, year; 
+  scanf("%d %d", &month, &year); 
+  printf("%d\n", month_length(month, year));   
+  return 0; 
+}
