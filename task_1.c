b6d7d8520c123ad1c8c6ada9fc3a89311f73b26a
Sam Komarov <sam@skomarov.com>
2023-05-01 15:22:39 +0300

    fix task #1 - add november to list of 31-day months

--- task_1.c
+++ task_1.c
@@ -4,7 +4,7 @@ int month_length(int month, int year) {
   if (month == 2) {
     int is_leap = !(year % 4) && (year % 100 || !(year % 400)); 
     return is_leap ? 29 : 28; 
-  } else if (month == 4 || month == 6 || month == 9) {
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
     return 30; 
   } else {
     return 31; 
